0afa0fea47531b2432b59965a2fb1fa405f6f494
Mijura <miodragvilotijevic@gmail.com>
2020-04-05 16:12:47 +0200

    created the test services

---
+++ spring-cloud-study/client-service-2/src/main/java/spring/cloud/study/app/ClientService2.java
@@ -0,0 +1,13 @@
+package spring.cloud.study.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientService2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientService2.class, args);
+	}
+
+}
